7ce3ead21e8073acc01bc039553ef698af864a32
Sylvernale <david.carlson2012@gmail.com>
2018-12-14 10:48:18 -0600

    Initial Project setup

---
+++ src/main/java/sylvernale/bank/App.java
@@ -0,0 +1,13 @@
+package sylvernale.bank;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
