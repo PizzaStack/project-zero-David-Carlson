5d65455a93e361b9413d19fb1cbc6a8db23df525
Sylvernale <david.carlson2012@gmail.com>
2018-12-17 12:44:38 -0600

    Added Account Tests

--- src/main/java/sylvernale/bank/entity/Account.java
+++ src/main/java/sylvernale/bank/entity/Account.java
@@ -1,5 +1,55 @@
 package sylvernale.bank.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Account {
+	protected List<User> owners;
+	protected double balance;
+	protected AccountType accountType;
+	public enum AccountType {
+		Credit, Debit
+	};
+	
+	public Account() {
+		owners = new ArrayList<User>();
+		accountType = AccountType.Debit;
+	}
+	
+	public Account(User owner, double startingBalance, AccountType accountType) {
+		this();
+		owners.add(owner);
+		balance = startingBalance;
+		this.accountType = accountType;
+	}
+	
+	public List<User> getOwners() {
+		return owners;
+	}
+
+	public boolean containsOwner(User user) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public Object getAccountType() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Object getBalance() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void changeAccountType(AccountType newType) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void addOwner(User user2) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
--- src/test/java/entitytest/AccountTest.java
+++ src/test/java/entitytest/AccountTest.java
@@ -1,12 +1,71 @@
 package entitytest;
 
 import org.junit.Test;
+
+import sylvernale.bank.entity.Account;
+import sylvernale.bank.entity.User;
+
 import static org.junit.Assert.*;
 
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+
 public class AccountTest {
-	@Test
-	public void testAddUser() {
-		
+	double balance = 1234;
+	Account.AccountType accountType = Account.AccountType.Credit;
+	User user;
+	Account account;
+	
+	@Before
+	public void setupNecessaryClasses() {
+		user = new User();		
+		account = new Account(user, balance, accountType);
 	}
 	
+	@Test 
+	public void constructAccount() {
+		Account account = new Account();
+		assertNotNull(account);
+	}
+	@Test 
+	public void constructAccountWithData() {
+		User user = new User();
+		double balance = 100;
+		Account.AccountType type = Account.AccountType.Credit;
+		Account account = new Account(user, balance, type);
+		assertNotNull(account);
+	}
+	
+	@Test
+	public void testGetOwners() {
+		
+	}
+	@Test
+	public void testAddOwner() {
+		User user2 = new User();
+		account.addOwner(user2);
+		List<User> users = account.getOwners();
+		assertTrue(users.contains(user2));
+	}
+	@Test 
+	public void testContainsUser() {
+		assertTrue(account.containsOwner(user));
+		assertFalse(account.containsOwner(new User()));
+	}
+	@Test
+	public void testGetAccountType() {
+		assertEquals(account.getAccountType(), accountType);
+	}
+	@Test
+	public void testGetBalance() {
+		assertEquals(account.getBalance(), balance);
+	}
+	
+	@Test
+	public void testChangeAccountType() {
+		Account.AccountType newType = Account.AccountType.Debit;
+		account.changeAccountType(newType);
+	}
 }
